0910532f12951ba0c864f975ceb6c31c1bb80422
delectation <wongyhpersonal@gmail.com>
2024-07-13 15:52:01 +0800

    Add support for integers in lexer

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Error information does not include filenames and line numbers
 Can be improved using io.Reader and filename
 Current implementation does not support full Unicode range due to byte (lexer.go:7) implementation
 Can be improved by using rune and adjusting methods handling read patterns
+Current implementation does not support floats due to lexer  implementation
--- lexer/lexer.go
+++ lexer/lexer.go
@@ -27,37 +27,53 @@ func (l *Lexer) readChar() {
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
+
 	switch l.ch {
-		case '=':
-			tok = newToken(token.ASSIGN, l.ch)
-		case ';':
-			tok = newToken(token.SEMICOLON, l.ch)
-		case '(':
-			tok = newToken(token.LPAREN, l.ch)
-		case ')':
-			tok = newToken(token.RPAREN, l.ch)
-		case ',':
-			tok = newToken(token.COMMA, l.ch)
-		case '+':
-			tok = newToken(token.PLUS, l.ch)
-		case '{':
-			tok = newToken(token.LBRACE, l.ch)
-		case '}':
-			tok = newToken(token.RBRACE, l.ch)
-		case 0:
-			tok.Literal = ""
-			tok.Type = token.EOF
-		default:
-			if isLetter(l.ch) {
-				tok.Literal = l.readIdentifier()
-				tok.Type = token.LookupIdentifier(tok.Literal)
-				return tok
-			} else {
-				tok = newToken(token.ILLEGAL, l.ch)
-			}
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdentifier(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
+}
+
+// this can be abstracted -> either via arg or interfaces
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
 }
 
 func (l *Lexer) readIdentifier() string {
@@ -68,12 +84,21 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
 // maybe should abstract this creation of token to something else
 func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal:string(ch)}
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
-	
