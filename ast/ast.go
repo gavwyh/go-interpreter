b2bdcda42fb439f7158a87ab2d5778dc8848d622
gavwyh <wongyhpersonal@gmail.com>
2024-07-16 20:50:18 +0800

    Add AST implementation for basic statements

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import 	"github.com/gavwyh/go-interpreter/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node 
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+// root node of every AST 
+type Program struct {
+	Statements []Statement
+}
+
+// Implements expression as identifiers can produce values -> anyth produces val = expression
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
