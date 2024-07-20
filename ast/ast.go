4022e007a4f8f6d455dc6a87ab9f5d630930a079
gavwyh <wongyhpersonal@gmail.com>
2024-07-20 15:39:53 +0800

    Add debugging and test cases for ast statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/gavwyh/go-interpreter/token"
+import (
+	"strings"
+
+	"github.com/gavwyh/go-interpreter/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -14,6 +19,11 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
 }
 
 type LetStatement struct {
@@ -27,28 +37,33 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
+type ExpressionStatement struct {
+	Token token.Token 
+	Expression Expression
+}
+
 // root node of every AST
 type Program struct {
 	Statements []Statement
 }
 
-// Implements expression as identifiers can produce values -> anyth produces val = expression
-type Identifier struct {
-	Token token.Token
-	Value string
-}
-
-func (ls *LetStatement) statementNode() {}
-func (ls *LetStatement) TokenLiteral() string {
-	return ls.Token.Literal
-}
-
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
+func (program *Program) TokenLiteral() string {
+	if len(program.Statements) > 0 {
+		return program.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
+
+// used instead of bytes.buffer for memory optimisation
+func (program *Program) String() string {
+	var out strings.Builder
+
+	for _, statement := range program.Statements {
+		out.WriteString(statement.String())
+	}
+
+	return out.String()
 }
 
 func (i *Identifier) expressionNode() {}
@@ -56,5 +71,54 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string { 
+	return i.Value 
+}
+
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out strings.Builder
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	
+	out.WriteString(";")
+	return out.String()
+}
+
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (rs *ReturnStatement) String() string {
+	var out strings.Builder
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	
+	out.WriteString(";")
+	return out.String()
+}
+
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
---
+++ ast/ast_test.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/gavwyh/go-interpreter/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "x"},
+					Value: "x",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENTIFIER, Literal: "y"},
+					Value: "y",
+				},
+			},
+		},
+	},
+
+	if program.String() != "let x = y;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
